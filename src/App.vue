ec5cf8105b13e452b5148038d426e2c95a2e39b5
Foster <foster@sandcastle.co>
2021-01-09 09:11:29 -0800

    changes, switch to personal site branding

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,62 @@
 <template>
   <div id="app">
-    i love transistor
-    <GameCard />
+    <b-container fluid>
+      <b-row no-gutters>
+        <b-col class="pt-3 text-center">
+          <h4 class="bold grey">Foster Douglas</h4>
+        </b-col>
+      </b-row>
+      <b-row no-gutters>
+        <ProjectCard v-for="project in 6" :key="project" />
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import GameCard from "./components/GameCard.vue";
+import ProjectCard from "./components/ProjectCard.vue";
 
 @Component({
   components: {
-    GameCard
+    ProjectCard
   }
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import url("https://use.typekit.net/hey1ekp.css");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "bitter", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
 
 //BOOTSTRAP OVERRIDES
+$white: #fff !default;
+$gray-100: #f8f9fa !default;
+$gray-200: #e9ecef !default;
+$gray-300: #dee2e6 !default;
+$gray-400: #ced4da !default;
+$gray-500: #adb5bd !default;
+$gray-600: #6c757d !default;
+$gray-700: #495057 !default;
+$gray-800: #343a40 !default;
+$gray-900: #212529 !default;
+$black: #000 !default;
+
 .container-fluid {
   padding: 0 !important;
+}
+
+//STYLES
+.bold {
+  font-weight: 700;
+}
+
+.grey {
+  color: $gray-700;
 }
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-col cols="12" md="6" xl="4" class="p-3">
+    <img class="projectCard" src="../assets/test.jpg" width="100%" />
+    <h6 class="text-center mt-1">test</h6>
+  </b-col>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ProjectCard extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="scss">
+.projectCard {
+  border-radius: 20rem;
+}
+</style>
